9bbcba1b582562daa5888bb8e2d0137157bcd00a
Jordan Facibene <jordan.facibene13@stjohns.edu>
2015-11-11 18:42:49 -0500

    Start compatibility patch for setup script
    Impacted Area: setup.sh
    
    Before this commit: No check for success of package manager command (apt-get). 
    
    After this commit: Exit status of 'sudo apt-get update' is stored in $EXITSTATUS. 'sudo apt-get install sshpass' is only executed if the value of $EXITSTATUS is not 127 (command not found)

--- setup.sh
+++ setup.sh
@@ -5,4 +5,13 @@ cp profile-setup.sh /home/$USER/.cend # copy files over to the .cend folder
 cp transfer.sh /home/$USER/.cend
 echo "alias cend='bash /home/$USER/.cend/transfer.sh'" >> /home/$USER/.bashrc  # create alias's for the scripts to be used 
 echo "alias cendProfile='bash /home/$USER/.cend/profile-setup.sh'" >> /home/$USER/.bashrc
-sudo apt-get install sshpass # install sshpass (obviously for debian/ubuntu)
+sudo apt-get update # update + used for testing for apt-get
+EXITSTATUS=$?
+if [ $EXITSTATUS -eq 127 ]; then
+  echo "manually build sshpass from source here"
+  # pull source
+  # make 
+  # etc... 
+else
+  sudo apt-get install sshpass # install sshpass (obviously for debian/ubuntu)
+fi
